7ab4598c590a31c7f8929afd26378ec98081f0ac
philokaram <146956626+philokaram@users.noreply.github.com>
2024-12-05 10:16:46 +0200

    compress done

--- ArithmeticCoding.java
+++ ArithmeticCoding.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
 public class ArithmeticCoding implements LosslessCompression{
 
     @Override
-    public String compress(String originalText) {
+    public double compress(String originalText) {
         //1- calculate the probabilities
-        
-        return "";
+        Map<Character,Double> probabilities  = Calculation.CalculateProbabilities(originalText);
+        //2- make array of ranges
+        RangeNode[] ranges = new RangeNode[probabilities.size()];
+        int index = 0;
+        double highRange = 0.0;
+        for (Entry<Character,Double> entry : probabilities.entrySet()) {
+            ranges[index] = new RangeNode(entry.getKey(),highRange,highRange+entry.getValue());
+            highRange += entry.getValue(); 
+        }
+        //3- compress
+        double low = 0;
+        double high = 0;
+        double range = 0;
+        for (Character ch : originalText.toCharArray()) {
+            for(int i = 0; i < ranges.size ; i++){
+                if(ch == ranges[i].ch){
+                    range = high - low;
+                    high = low + (range * ranges[i].highRange);
+                    low = low + (range * ranges[i].lowRange);
+                    break;
+                }
+            }
+        }
+        //4- select number from last range
+        return (high-low)/2;
     }
 
     @Override
---
+++ RangeNode.java
@@ -0,0 +1,10 @@
+public class RangeNode {
+    public Character ch;
+    public double  lowRange;
+    public double  highRange;
+    public RangeNode(Character ch, double lowRange, double highRange) {
+        this.ch = ch;
+        this.lowRange = lowRange;
+        this.highRange = highRange;
+    }
+}
